5aeec595d74d3bd293e26fcd0bf9a8fb0936add2
Flavio Cesar <flaviocesarnofrontend@gmail.com>
2025-12-04 15:12:49 -0300

    pagina servicos com menu mobile

---
+++ assets/js/global.js
@@ -0,0 +1,9 @@
+//script para gerenciar o menu hamburguer na versao mobile
+console.log('%cJavascript activated!', 'color: lightgreen; background-color: blue;');
+
+const navToggleBtn = document.querySelector('.nav-toggle-btn');
+const nav = document.querySelector('.nav');
+
+navToggleBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible');
+})
